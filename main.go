c4461f33f001bd2a61a0b02b9a72a3f4caf0cd87
Dylan Meeks <dylan.meeks@aol.com>
2025-02-12 19:52:20 +0800

    feat: add test and repl, make module

--- main.go
+++ main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
 
 func main() {
-    fmt.Println("Hello World")
+
+	commands := map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+
+	sc := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		sc.Scan()
+		input := sc.Text()
+		input = strings.ToLower(input)
+		words := strings.Fields(input)
+        
+        if command, exists := commands[words[0]]; exists {
+            command.callback()
+        } else {
+            fmt.Println("Unknown command")
+        }
+	}
+}
+
+func cleanInput(text string) []string {
+	words := strings.Fields(text)
+	return words
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
 }
