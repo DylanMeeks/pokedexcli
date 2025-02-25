e495df07e96395a74c7d51eff97b1b69770a9309
Dylan Meeks <dylan.meeks@aol.com>
2025-02-25 16:38:47 +0800

    chore: PLEASE CACHE WHHHHY

--- internal/pokecache/cache.go
+++ internal/pokecache/cache.go
@@ -30,32 +30,38 @@ func (C Cache) reapLoop(interval time.Duration) {
     ticker := time.NewTicker(interval)
     for range ticker.C {
         C.mutex.Lock()
+        fmt.Printf("reapLoop Lock")
         for k, v := range C.cache {
             if time.Now().Sub(v.createdAt) > interval {
                 delete(C.cache, k)
             }
         }
         C.mutex.Unlock()
+        fmt.Printf("reapLoop Unlock")
     }
 }
 
 func (C Cache) Add(key string, val []byte) {
     fmt.Printf("waiting for lock")
     C.mutex.Lock()
+    fmt.Printf("add Lock")
 	C.cache[key] = cacheEntry{
 		createdAt: time.Now(),
 		val:       val,
 	}
     C.mutex.Unlock()
+    fmt.Printf("add unlock")
 }
 
 func (C Cache) Get(key string) ([]byte, bool) {
     C.mutex.Lock()
+    fmt.Printf("get Lock")
     entry, ok := C.cache[key]
     if !ok {
         return []byte{}, ok
     }
     val := entry.val
     C.mutex.Unlock()
+    fmt.Printf("get unlock")
     return val, ok
 }
